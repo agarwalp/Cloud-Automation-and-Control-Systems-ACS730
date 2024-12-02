6f1cf214cfeab400a3debaa5b1413981f180b59c
agarwalp <p19.agarwal@gmail.com>
2024-12-02 18:02:16 -0500

    pullign latest network module

---
+++ network/config.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "acs730finalproj-dev"                          # S3 bucket to SAVE Terraform State
+    key    = "environment/dev/network_module/terraform.tfstate" # Object key for network state
+    region = "us-east-1"                                    # Region where bucket is created
+  }
+}
