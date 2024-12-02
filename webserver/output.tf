1a96d4f75958c4bc1499c8a868a2ef942af25c0c
PoojaSekar2405 <poojasekar2405@gmail.com>
2024-12-02 07:31:38 +0530

    Pulled webserver folder from Dev into pooja-workspace

--- webserver/output.tf
+++ webserver/output.tf
@@ -1,4 +1,11 @@
-#ALB's DNS name
-output "ALB_dns_name" {
-  value       = aws_lb.application_load_balancer.dns_name
+output "asgName" {
+  value = aws_autoscaling_group.WebServerASG.name
+}
+
+output "albDnsName" {
+  value = aws_lb.WebAlb.dns_name
+}
+
+output "webSgId" {
+  value = aws_security_group.WebServerSG.id
 }
--- webserver/variables.tf
+++ webserver/variables.tf
@@ -1,44 +1,64 @@
-variable "default_tags" {
-  default     = {}
-  type        = map(any)
-  description = "Default tags"
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
-variable "instance_type" {
-
-  description = "Type of the instance"
+variable "vpcId" {
+  description = "VPC ID"
   type        = string
 }
 
-variable "env" {
+variable "publicSubnets" {
+  description = "Public subnet IDs"
+  type        = list(string)
+}
 
+variable "amiId" {
+  description = "AMI ID for the webserver"
   type        = string
-  description = "Deployment Environment"
+  default     = "ami-0c02fb55956c7d316"  
 }
 
-#Minimum number of EC2 atleast present
-variable "minimum_size" {
+variable "instanceType" {
+  description = "Instance type for the webserver"
+  type        = string
+   default     = "t2.micro"
+}
 
+variable "desiredCapacity" {
+  description = "Desired capacity for ASG"
   type        = number
-  description = "Minimum capacity"
+  default  =   4
 }
 
-#Desired number of EC2 in auto scaling group
-variable "desired_size" {
-
+variable "maxSize" {
+  description = "Max size for ASG"
   type        = number
-  description = "Desired capacity"
+  default     = 4
 }
 
-#Minimum number of EC2 instances that can spinned up
-variable "maximum_size" {
-
+variable "minSize" {
+  description = "Min size for ASG"
   type        = number
-  description = "Maximum capacity"
+  default     = 1
 }
 
-variable "path_to_ssh_key" {
+variable "allowedHttpIps" {
+  description = "Allowed IP ranges for HTTP"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
 
-  description = "Path to the public ssh key"
+variable "allowedSshIps" {
+  description = "Allowed IP ranges for SSH"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "environment" {
+  description = "Environment name"
   type        = string
 }
+
+
