758966fba724e61078e3f54beac1d1c7f5ada286
Arjoo-Khattri <v2002arju@gmail.com>
2024-12-06 08:42:52 -0500

    Updating config.tf with correction

--- network_module/config.tf
+++ network_module/config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "acs730finalproj-dve"                              # S3 bucket to SAVE Terraform State
+    bucket = "acs730finalproj-dev"                              # S3 bucket to SAVE Terraform State
     key    = "environment/dev/network_module/terraform.tfstate" # Object key for network state
     region = "us-east-1"                                        # Region where bucket is created
   }
