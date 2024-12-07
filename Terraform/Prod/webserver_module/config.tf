24f4b368512406e349c35cf18559e03cb3016068
agarwalp <p19.agarwal@gmail.com>
2024-12-06 22:48:10 -0500

    prod deployement code

---
+++ Terraform/Prod/webserver_module/config.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.3.0" # Specify the required Terraform version
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0, < 5.0.0" # Specify the AWS provider version
+    }
+  }
+
+  backend "s3" {
+    bucket = "group1-acs730finalproject-prod"
+    key    = "prod/webserver_module/terraform.tfstate" # Key for state file
+    region = "us-east-1"                               # Region for the S3 bucket
+  }
+}
